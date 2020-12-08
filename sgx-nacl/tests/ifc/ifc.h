c0da9db311cb5c25e71f06c2dd098f36bb58c73a
KangMinHyeok <saigoyo@nate.com>
2020-12-08 10:05:09 +0900

    add copyright

--- sgx-nacl/tests/ifc/ifc.h
+++ sgx-nacl/tests/ifc/ifc.h
@@ -1,4 +1,8 @@
-
+/*
+ * Copyright (c) 2020 The SandboxTEE Authors. All rights reserved.
+ * Use of this source code is governed by a GPLv2 license that can be
+ * found in the LICENSE file.
+*/
 
 #include <sys/types.h>
 
