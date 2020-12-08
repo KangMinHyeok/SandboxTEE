dd53eab6bebbcf1d33b9c6acbc753ddb66a1316a
KangMinHyeok <saigoyo@nate.com>
2020-12-08 09:55:45 +0900

    add copyright

--- sgx-nacl/tests/ifc/network_analyzer.c
+++ sgx-nacl/tests/ifc/network_analyzer.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2020 The SandboxTEE Authors. All rights reserved.
+ * Use of this source code is governed by a GPLv2 license that can be
+ * found in the LICENSE file.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
