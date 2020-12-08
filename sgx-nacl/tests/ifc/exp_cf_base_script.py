0653b644b2192c3c17029d0f29144be18b350026
KangMinHyeok <saigoyo@nate.com>
2020-12-08 09:55:24 +0900

    add copyright

--- sgx-nacl/tests/ifc/exp_cf_base_script.py
+++ sgx-nacl/tests/ifc/exp_cf_base_script.py
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2020 The SandboxTEE Authors. All rights reserved.
+ * Use of this source code is governed by a GPLv2 license that can be
+ * found in the LICENSE file.
+*/
+
 import subprocess
 import os
 from multiprocessing import Process
