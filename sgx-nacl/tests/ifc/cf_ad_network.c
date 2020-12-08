7b96dee6516af483e5bfa434b27aa4ef198e0e48
KangMinHyeok <saigoyo@nate.com>
2020-12-08 09:53:59 +0900

    add copyrights

--- sgx-nacl/tests/ifc/cf_ad_network.c
+++ sgx-nacl/tests/ifc/cf_ad_network.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2020 The SandboxTEE Authors. All rights reserved.
+ * Use of this source code is governed by a GPLv2 license that can be
+ * found in the LICENSE file.
+*/
+
 #define FLT_RADIX 2
 
 #include <stdio.h>
