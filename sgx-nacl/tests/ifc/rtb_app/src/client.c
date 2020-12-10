e10d8107ee231fd8ddbe114db04d4e0cb0cad196
Chorom Hamm <choromh@snu.ac.kr>
2020-12-10 21:41:47 +0900

    add logic

--- sgx-nacl/tests/ifc/rtb_app/src/client.c
+++ sgx-nacl/tests/ifc/rtb_app/src/client.c
@@ -103,7 +103,6 @@ int main(int argc, char *argv[]) {
 		ad_id = (char *)malloc(len * sizeof(char)+1);
 		memcpy(ad_id, buffer_in+sizeof(bid)+sizeof(len), len+1);
 
-		/*
 		n = read(sockfd, &len, sizeof(len));
 		if (n < 0) {
 			printf("Read error\n");
@@ -117,7 +116,6 @@ int main(int argc, char *argv[]) {
 			printf("Read error\n");
 			goto exit;
 		}
-		*/
 		printf("Accepted Bid %.9f - AD id: %s\n\n", bid, ad_id);
 
 		count++;
