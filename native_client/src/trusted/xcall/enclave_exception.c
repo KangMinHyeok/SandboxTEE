bfc4b33731e5462e2c65427b4ec08443596f87e9
hmlee <hmlee@mmlab.snu.ac.kr>
2021-07-28 17:43:52 +0900

    enclave_exception

---
+++ native_client/src/trusted/xcall/enclave_exception.c
@@ -0,0 +1,22 @@
+#include "native_client/src/trusted/xcall/enclave_exception.h"
+
+// TODO
+
+void ExceptionHandler (unsigned int exit_info, void * uc){
+  UNREFERENCED_PARAMETER(exit_info);
+  UNREFERENCED_PARAMETER(uc);
+}
+
+void HandleExternalEvent (int event, void * uc){
+  UNREFERENCED_PARAMETER(uc);
+  UNREFERENCED_PARAMETER(event);
+}
+
+/* This does not return */
+void __abort(void) {
+}
+
+void warn (const char *format, ...)
+{
+
+}
---
+++ native_client/src/trusted/xcall/enclave_exception.h
@@ -0,0 +1,7 @@
+#include "native_client/src/include/nacl_compiler_annotations.h"
+
+#ifndef _ENCLAVE_EXCEPTION_H_
+#define _ENCLAVE_EXCEPTION_H_ 1
+void ExceptionHandler (unsigned int exit_info, void * uc);
+
+#endif
