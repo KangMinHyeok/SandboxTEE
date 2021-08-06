83c750e6a088f5d4399d09f891c220498c4c4e40
hmlee <hmlee@mmlab.snu.ac.kr>
2021-08-06 18:36:53 +0900

    wolfMQTT import

---
+++ native_client/src/trusted/wolfMQTT/wolfmqtt/options.h
@@ -0,0 +1,31 @@
+/* wolfmqtt options.h
+ * generated from configure options
+ *
+ * Copyright (C) 2006-2020 wolfSSL Inc.
+ *
+ * * This file is part of wolfMQTT.
+ *
+ */
+
+#ifndef WOLFMQTT_OPTIONS_H
+#define WOLFMQTT_OPTIONS_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#undef  SIZEOF_LONG
+#define SIZEOF_LONG 8
+
+#undef  WOLFMQTT_DISCONNECT_CB
+#define WOLFMQTT_DISCONNECT_CB
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* WOLFMQTT_OPTIONS_H */
+
