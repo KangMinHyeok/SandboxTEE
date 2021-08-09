c8c6c0caf47a9b45e3a7d06440db1c65e18ba787
KangMinHyeok <alsgur434@gamil.com>
2021-08-09 22:29:39 +0900

    define stdin/out/err as 0, 1, 2

--- native_client/src/trusted/stdlib/stdio.h
+++ native_client/src/trusted/stdlib/stdio.h
@@ -1,13 +1,16 @@
-#include "native_client/src/trusted/stdlib/files.h"
+#include "native_client/src/trusted/stdlib/fileio.h"
+#include "native_client/src/include/build_config.h"
 
+#if NACL_SGX == 1
 /* Standard streams.  */
-//extern FILE *stdin;                /* Standard input stream.  */
-//extern FILE *stdout;                /* Standard output stream.  */
-//extern FILE *stderr;                /* Standard error output stream.  */
+extern FILE *stdin;                /* Standard input stream.  */
+extern FILE *stdout;                /* Standard output stream.  */
+extern FILE *stderr;                /* Standard error output stream.  */
 /* C89/C99 say they're macros.  Make them happy.  */
-//#define stdin stdin
-//#define stdout stdout
-//#define stderr stderr
+#define stdin stdin
+#define stdout stdout
+#define stderr stderr
+#endif 
 
 #define EOF (-1)
 
