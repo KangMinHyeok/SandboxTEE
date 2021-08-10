0c22d4c6127cd8ab1f2248d967b35547b4605dba
KangMinHyeok <alsgur434@gamil.com>
2021-08-10 10:11:19 +0900

    add headers

--- native_client/src/trusted/stdlib/fileio.c
+++ native_client/src/trusted/stdlib/fileio.c
@@ -1,9 +1,14 @@
-
 #include "native_client/src/trusted/stdlib/fileio.h"
+#include "native_client/src/trusted/stdlib/filestruct.h"
 #include "native_client/src/trusted/xcall/enclave_ocalls.h"
+#include "native_client/src/include/build_config.h"
+
+#if NACL_SGX == 1
 
 int fflush(FILE *fp)
 {
     int ret = ocall_fflush(fp);
     return ret;
 }
+
+#endif
