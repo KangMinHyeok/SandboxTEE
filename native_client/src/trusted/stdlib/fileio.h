2c9a9ea73abda4062a6722466c146f1d0a6c4883
KangMinHyeok <alsgur434@gamil.com>
2021-08-10 16:05:39 +0900

    add file io functions

--- native_client/src/trusted/stdlib/fileio.h
+++ native_client/src/trusted/stdlib/fileio.h
@@ -1,11 +1,20 @@
 #ifndef _FILEIO_H_
 #define _FILEIO_H_
 #include "native_client/src/trusted/stdlib/filestruct.h"
+#include "native_client/src/trusted/stdlib/stddef.h"
 #include "native_client/src/include/build_config.h"
 
 #if NACL_SGX == 1
 
 int fflush(FILE *fp);
+int fsync(int fd);
+int ferror(FILE *fp);
+long ftell(FILE *fp);
+int fseek(FILE *fp, long offset, int whence);
+FILE * fopen(const char *file, const char *mode);
+int fclose(FILE *fp);
+size_t fread(void *buf, size_t size, size_t count, register FILE *fp);
+size_t fwrite(const void *buf, size_t size, size_t count, register FILE *fp);
 #endif
 
 #endif
