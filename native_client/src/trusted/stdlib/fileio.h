68e4c4468e6efa2ef759a042d12c2137bc990af5
KangMinHyeok <alsgur434@gamil.com>
2021-08-10 12:05:15 +0900

    move FILE structure and add headers

--- native_client/src/trusted/stdlib/fileio.h
+++ native_client/src/trusted/stdlib/fileio.h
@@ -1,24 +1,12 @@
 #ifndef _FILEIO_H_
 #define _FILEIO_H_
+#include "native_client/src/trusted/stdlib/filestruct.h"
+#include "native_client/src/include/build_config.h"
 
-#ifndef __FILE_defined
-#define __FILE_defined 1
-typedef struct 
-{
-  short level ;
-  short token ;
-  short bsize ;
-  char fd ;
-  unsigned flags ;
-  unsigned char hold ;
-  unsigned char *buffer ;
-  unsigned char * curp ;
-  unsigned istemp; 
-}FILE ;
-
-#endif
+#if NACL_SGX == 1
 
 int fflush(FILE *fp);
+#endif
 
 #endif
 
