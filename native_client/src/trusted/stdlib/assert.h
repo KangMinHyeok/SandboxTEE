09ee973aecfc003e4087a37e8db25bb78fd15b54
KangMinHyeok <alsgur434@gamil.com>
2021-08-11 23:13:49 +0900

    mhkang

--- native_client/src/trusted/stdlib/assert.h
+++ native_client/src/trusted/stdlib/assert.h
@@ -21,7 +21,8 @@
 
 void warn (const char *format, ...);
 void __abort(void);
-
+# ifdef assert
+# undef assert
 # define assert(test)                                                   \
     ({                                                                  \
         long _val = (long) (test);                                      \
@@ -32,5 +33,65 @@ void __abort(void);
                     __abort(); })                                       \
             : (void) 0;                                                 \
     })
+#else
+
+  /*
+void
+__assert_fail_base (const char *fmt, const char *assertion, const char *file,
+		    unsigned int line, const char *function)
+{
+  char *str;
+
+  int total;
+  if (__asprintf (&str, fmt,
+		  __progname, __progname[0] ? ": " : "",
+		  file, line,
+		  function ? function : "", function ? ": " : "",
+		  assertion, &total) >= 0)
+    {
+      // Print the message.  /
+      (void) __fxprintf (NULL, "%s", str);
+      (void) fflush (stderr);
+
+      total = (total + 1 + GLRO(dl_pagesize) - 1) & ~(GLRO(dl_pagesize) - 1);
+      struct abort_msg_s *buf = __mmap (NULL, total, PROT_READ | PROT_WRITE,
+					MAP_ANON | MAP_PRIVATE, -1, 0);
+      if (__glibc_likely (buf != MAP_FAILED))
+	{
+	  buf->size = total;
+	  strcpy (buf->msg, str);
+
+	  // We have to free the old buffer since the application might
+ 	  //   catch the SIGABRT signal.
+	  struct abort_msg_s *old = atomic_exchange_acq (&__abort_msg, buf);
+
+	  if (old != NULL)
+	    __munmap (old, old->size);
+	}
+
+      free (str);
+    }
+  else
+    {
+      // At least print a minimal message.  /
+      static const char errstr[] = "Unexpected error.\n";
+      __libc_write (STDERR_FILENO, errstr, sizeof (errstr) - 1);
+    }
+  abort ();
+  // TODO: impl
+}
+
+
+#undef __assert_fail
+void
+__assert_fail (const char *assertion, const char *file, unsigned int line,
+	       const char *function)
+{
+  __assert_fail_base ("%s%s%s:%u: %s%sAssertion `%s' failed.\n%n", assertion, file, line, function);
+	__abort();
+}
+
+  */
+#endif 
 
 #endif
