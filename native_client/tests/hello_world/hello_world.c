1ba64a570f956fbf94b986b3c5fc37d3969dd6fa
hmlee <hmlee@mmlab.snu.ac.kr>
2021-11-02 16:41:02 +0900

    hmlee hello world

--- native_client/tests/hello_world/hello_world.c
+++ native_client/tests/hello_world/hello_world.c
@@ -9,9 +9,19 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 void hello_world(void) {
   printf("Hello, World!\n");
+	int f = open("/home/hmlee/SandboxTEE/native_client/tests/hello_world/testdata256.txt", O_CREAT, 0644);
+  if (f < 0) {
+    printf("open fail\n");
+  } else {
+  	printf("open success\n");
+  }
+  close(f);
 }
 
 int main(int argc, char* argv[]) {
