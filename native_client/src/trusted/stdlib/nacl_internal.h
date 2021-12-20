4f65edd7425e6a0179faebd9453ec25c5375e0c3
mkpark <mkpark@mmlab>
2021-12-20 19:57:13 +0900

    clean defines

--- native_client/src/trusted/stdlib/nacl_internal.h
+++ native_client/src/trusted/stdlib/nacl_internal.h
@@ -30,33 +30,8 @@
 #include "native_client/src/include/build_config.h"
 #include "list.h"
 
-/* (Linux-only) enable caching loaded binaries for optimizing process creation */
-#define CACHE_LOADED_BINARIES    0      /* default: disabled */
-
 /* statically allocate slab manager */
 #define STATIC_SLAB              1
-
-/* maximum length of URIs */
-#define URI_MAX                  256
-
-/* allow binding sockets to ANY addresses (e.g., 0.0.0.0:0) */
-#define ALLOW_BIND_ANY           1
-
-/* turn on the following option to trace heap memory leak */
-#define TRACE_HEAP_LEAK
-
-#ifdef PROFILING
-// #define PROFILING 1
-#else
-#undef PROFILING
-#define PROFILING 0
-#endif
-
-/*TODO crhamm
-#ifndef IN_ENCLAVE
-#error "nacl_internal.h can only be included in NACL"
-#endif
-*/
 
 #include <stdbool.h>
 #include <stddef.h>
